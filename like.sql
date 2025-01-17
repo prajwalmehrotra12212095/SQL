b030f70befa7edb708600d80fbe7e05287cbf475
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-01-17 20:18:40 +0530

    learn

---
+++ like.sql
@@ -0,0 +1,8 @@
+--------LIKE
+SELECT *
+FROM students
+SELECT student_fname,student_id, enrollment_date, selected_course, years_of_exp, student_company, batch_date
+--FROM students
+--WHERE student_fname LIKE 'ra%';
+from students
+WHERE student_fname LIKE '____';
