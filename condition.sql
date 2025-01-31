331e8cf539c4bee105697b4ffec834e2eeb926cb
prajwalmehrotra12212095 <prajwalmehrotra234@gmail.com>
2025-02-01 03:24:11 +0530

    mesage

--- condition.sql
+++ condition.sql
@@ -4,3 +4,12 @@ WHEN course_duration_months > 4 THEN 'masters'
 ELSE 'diploma'
 END AS course_type
 from courses
+
+select *,
+CASE 
+WHEN student_company in('walmart','microsoft') THEN 'product based'
+else 'service based'
+END AS company_type
+from students
+
+--we can add mutiple condition
